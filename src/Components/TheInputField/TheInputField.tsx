7a7081a4fe4a1de29aafc47b9762a58fcf65c485
naathaliie <nathalielarsson@live.se>
2024-04-11 12:17:30 +0200

    TheInputField-el clarooo yaas

--- src/Components/TheInputField/TheInputField.tsx
+++ src/Components/TheInputField/TheInputField.tsx
@@ -13,6 +13,18 @@ const TheInputField = () => {
   const [inputValue, setInputValue] = useState<string>("");
   //Ändra texten på knappen om den är aktiv eller inte
   const [btnText, setBtnText] = useState<string>("Inaktiv");
+  const [collectedInputs, setCollectedinputs] = useState<string[]>([]);
+
+  const handleClick = () => {
+    setCollectedinputs([...collectedInputs, inputValue]);
+    setInputValue("");
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      handleClick(); // Anropa handleClick när Enter trycks
+    }
+  };
 
   return (
     <div className="TheInputField">
@@ -26,18 +38,25 @@ const TheInputField = () => {
             setInputValue(value);
             setBtnText(value !== "" ? "Aktiv" : "Inaktiv");
           }}
+          onKeyDown={handleKeyDown} // Lägg till onKeyDown-händelsen
         />
       </div>
 
       <div className="btn-box">
-        <button className={`btn `} disabled={inputValue === ""}>
+        <button
+          className={`btn `}
+          disabled={inputValue === ""}
+          onClick={handleClick}
+        >
           {btnText}
         </button>
       </div>
 
       <div className="li-box">
         <ul>
-          <li>en punkt</li>
+          {collectedInputs.map((input, i) => {
+            return <li key={i}>{input}</li>;
+          })}
         </ul>
       </div>
     </div>
