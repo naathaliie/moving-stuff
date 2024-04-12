2baf3c804a4411aab24b1b7c1fa57361a690ba05
naathaliie <nathalielarsson@live.se>
2024-04-12 14:22:19 +0200

    progressBar

--- src/Components/ProgressBar/ProgressBar.tsx
+++ src/Components/ProgressBar/ProgressBar.tsx
@@ -5,17 +5,25 @@ Klickar man ur animerar vi att baren krymper ner ett steg. */
 
 import { useReducer, useState } from "react";
 import "./ProgressBar.scss";
-import { progressBarReducer } from "./progressBarReducer";
+import { ACTIONS, progressBarReducer } from "./progressBarReducer";
 
 const ProgressBar = () => {
-  const [width, setWidth] = useState<number>(0);
   const [state, dispatch] = useReducer(progressBarReducer, { progress: 0 });
+
+  //Kontrollerar om en checkbox blivit klickad/urklickad
+  const [checked_A, setchecked_A] = useState(false);
+  const [checked_B, setchecked_B] = useState(false);
+  const [checked_C, setChecked_C] = useState(false);
+  const [checked_D, setChecked_D] = useState(false);
 
   return (
     <div className="ProgressBar">
       <h1>ProgressBar</h1>
       <div className={`theProgressbar-Box`}>
-        <div className="theProgress" style={{ width: width }}></div>
+        <div
+          className="theProgress"
+          style={{ width: `${state.progress}%` }}
+        ></div>
       </div>
       <div className="checkbox-Box">
         <div className="Done25">
@@ -23,29 +31,69 @@ const ProgressBar = () => {
             <input
               type="checkbox"
               id="Done25"
+              checked={checked_A}
               onClick={() => {
-                setWidth("25%");
+                setchecked_A(!checked_A);
+                if (checked_A === false) {
+                  dispatch({ type: ACTIONS.ADD, payload: 25 });
+                } else {
+                  dispatch({ type: ACTIONS.REMOVE, payload: 25 });
+                }
               }}
             />
-            25% avklarat
+            A
           </label>
         </div>
         <div className="Done50">
           <label htmlFor="Done50">
-            <input type="checkbox" id="Done50" />
-            50% avklarat
+            <input
+              type="checkbox"
+              id="Done50"
+              checked={checked_B}
+              onClick={() => {
+                setchecked_B(!checked_B);
+                if (checked_B === false) {
+                  dispatch({ type: ACTIONS.ADD, payload: 25 });
+                } else {
+                  dispatch({ type: ACTIONS.REMOVE, payload: 25 });
+                }
+              }}
+            />
+            B
           </label>
         </div>
         <div className="Done75">
           <label htmlFor="Done75">
-            <input type="checkbox" id="Done75" />
-            75% avklarat
+            <input
+              type="checkbox"
+              id="Done75"
+              onClick={() => {
+                setChecked_C(!checked_C);
+                if (checked_C === false) {
+                  dispatch({ type: ACTIONS.ADD, payload: 25 });
+                } else {
+                  dispatch({ type: ACTIONS.REMOVE, payload: 25 });
+                }
+              }}
+            />
+            C
           </label>
         </div>
         <div className="Done100">
           <label htmlFor="Done100">
-            <input type="checkbox" id="Done100" />
-            100% avklarat
+            <input
+              type="checkbox"
+              id="Done100"
+              onClick={() => {
+                setChecked_D(!checked_D);
+                if (checked_D === false) {
+                  dispatch({ type: ACTIONS.ADD, payload: 25 });
+                } else {
+                  dispatch({ type: ACTIONS.REMOVE, payload: 25 });
+                }
+              }}
+            />
+            D
           </label>
         </div>
       </div>
--- src/Components/ProgressBar/progressBarReducer.ts
+++ src/Components/ProgressBar/progressBarReducer.ts
@@ -1,15 +1,15 @@
 /* Gör en egen typ för State så att den är statisk och endast kan innehålla progress som är ett nummer */
-type State = {
+ export type State = {
     progress:number,
 };
 
 /* Gör en egen typ för Action med en unionType, vilket innebär att Action kan vara antingen det ena eller det andra*/
-type Action = 
+export type Action = 
     |{type: typeof ACTIONS.ADD; payload:number}
     |{type: typeof ACTIONS.REMOVE; payload:number};
 
 //Skapar upp alla olika val jag vill kunna använda som action i switch-satsen
-const ACTIONS = {
+export const ACTIONS = {
     ADD: "add",
     REMOVE:"remove",
 };
@@ -19,9 +19,9 @@ En reducer tar alltid emot två properties, state (det nuvarande statet) samt en
 export const progressBarReducer = (state:State, action: Action) => {
     switch (action.type) {
         case ACTIONS.ADD:
-           return {state: state.progress + action.payload};
+           return {progress: state.progress + action.payload};
         case ACTIONS.REMOVE:
-            return {state: state.progress - action.payload};
+            return {progress: state.progress - action.payload};
         default:
             return state;
     }
